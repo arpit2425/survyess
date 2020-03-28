cf5ecd10843632f0a17179b222843781524cd1b1
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-28 16:48:50 +0530

    Deployement

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientId: process.env.googleClientId,
+  googleClientSecret: process.env.googleClientSecret,
+  database: process.env.database,
+  cookieKey: process.env.cookieKey
+};
--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,8 @@ passport.use(
     {
       clientID: keys.googleClientId,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
