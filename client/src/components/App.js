2ef9849ca138b34f7cbf9b994a37f593d77948b6
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-29 18:04:19 +0530

    Redux setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { BrowserRouter, Route } from "react-router-dom";
+const header = () => <div>Header</div>;
+const Dashboard = () => <div>Dashboard</div>;
+const surveyNew = () => <div>surveyNew</div>;
+const Landing = () => <div>Landing</div>;
 function App() {
   return <div>Hi there</div>;
 }
