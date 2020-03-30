649565468b930ec0957cbc779d2d38b99580e474
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-03-30 19:13:53 +0530

    Redux thunk

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-const header = () => <div>Header</div>;
-const Dashboard = () => <div>Dashboard</div>;
-const surveyNew = () => <div>surveyNew</div>;
-const Landing = () => <div>Landing</div>;
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./Header";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const surveyNew = () => <h2>surveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 function App() {
-  return <div>Hi there</div>;
+  return (
+    <div className="container">
+      <Router>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys/new" component={surveyNew} />
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
