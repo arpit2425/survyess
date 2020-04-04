3511e00635b83f8b060c0d65c8c45f72c643d47f
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-04-04 20:21:53 +0530

    mailer boilerplate

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,4 +1,27 @@
 const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("./../config/keys");
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+    this.from_email = new helper.Email("no-reply@surveyss.com");
+    this.subject = subject;
+    this.body = new helper("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
+  addClickTracking() {
+    const trackingSettings = new helper.trackingSettings();
+    const clickTracking = new helper.clickTracking(true, true);
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+}
+module.exports = Mailer;
--- services/emailTempletes/surveyTemplete.js
+++ services/emailTempletes/surveyTemplete.js
@@ -1,3 +1,3 @@
 module.exports = (survey) => {
-  return "<div>Hello</div>";
+  return "<div>" + survey.body + "</div>";
 };
