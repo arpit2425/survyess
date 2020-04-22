398a60d3095d6ee16b17b28b030b3d4c384030ce
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-04-22 20:55:52 +0530

    wiring surveys reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { FETCH_USER } from "./types";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 export const fetchUser = () => async (dispatch) => {
   const res = await axios.get("/api/user");
   dispatch({ type: FETCH_USER, payload: res.data });
@@ -13,4 +13,9 @@ export const submitSurvey = (values, histroy) => async (dispatch) => {
   histroy.push("/surveys");
 
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async (dispatch) => {
+  const surveys = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: surveys });
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import { reducer as reduxForm } from "redux-form";
+import surveyReducer from "./surveyReducer";
 export default combineReducers({
   auth: authReducer,
   form: reduxForm,
+  survey: surveyReducer,
 });
